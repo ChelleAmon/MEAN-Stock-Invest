40bc9337080a68a1edcbdfda8d185817f3b88043
Gichelle Amon <amongichelle@gmail.com>
2021-11-03 14:17:44 -0700

    chor(client,server): set up angular and mongoose

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,14 +1,27 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import mongoose from "mongoose";
 
 const app = express();
+const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/Invest-Stocks')
+.then(()=> console.log('DB connected successfully'))
+.catch((err) => console.log('Failed to connect to DB', err))
+
+
+app.use(cors());
+app.use(express.json());
+
+
 app.get('/', function(req, res) {
   console.log('app')
   res.json({test: 'tes2'})
 });
 
-app.listen(3333, function() {
-  console.log(`running on http://localhost:3333`)
+app.listen(PORT, function() {
+  console.log(`running on http://localhost:${PORT}`)
 }) 
 
 
