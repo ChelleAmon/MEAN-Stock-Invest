3dac68e002c59962266d5834151cfd082dcb5eca
Gichelle Amon <amongichelle@gmail.com>
2021-11-15 09:30:03 -0800

    feat(client): add search page and function

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './modules/components/search/search.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: "Let's-get-started", loadChildren :
-  () => import('./modules/user/user.module').then(m => m.UserModule)}
+  () => import('./modules/user/user.module').then(m => m.UserModule)},
+  {path: "Home-Search", component: SearchComponent}
 
 ];
 
--- src/client/src/app/modules/components/search/search.component.ts
+++ src/client/src/app/modules/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  searchForm: FormGroup;
+
+  myControl  = new FormControl();
+  options: string[] = ['BTC', 'ETH', 'DOGE', 'MANA', 'NPXS', 'DASH'];
+  filteredOptions!: Observable<any>;
+  search?: string;
+  selected: boolean = false;
+  name: any;
+  data: any;
+  err: any;
+  cArr: any;
+  inputSearch: any;
+
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      search: ['', Validators.required]
+    });
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(),
+      map(value => this._filter(value))
+    )
+   }
 
   ngOnInit(): void {
   }
 
+  private _filter(value:string): string[]{
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0)
+  }
+
+  searchFor(){
+    this.name = (this.searchForm.value.search);
+    this.selected = true;
+  }
 }
