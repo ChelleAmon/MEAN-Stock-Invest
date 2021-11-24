ea31305389f6475893a8f0586bc3de24a22ab67b
Gichelle Amon <amongichelle@gmail.com>
2021-11-24 12:29:22 -0800

    mongo issue- deploy

--- src/server/server.ts
+++ src/server/server.ts
@@ -21,7 +21,7 @@ const PORT =  process.env.PORT || 3000;
 const clientPath = path.join(__dirname, '/dist/client')
 app.use(express.static(clientPath))
 
-console.log(`${process.env.MONGOURI}`)
+console.log(`${process.env.MONGOURI}`);
 
 mongoose.connect(
   // 'mongodb://localhost:27017/Invest-Stocks',
